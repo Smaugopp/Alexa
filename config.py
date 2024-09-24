63b0c3831f2d5f53b3a9f340d5f6194cb0083cc4
Smaugopp <163888260+Smaugopp@users.noreply.github.com>
2024-09-24 14:48:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Smaugopp/Alice",
+    "https://github.com/Smaugopp/Alexa",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
